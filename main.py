389d9ed12e3566389dbbd75af49ba4d21029d59d
Serhii Maiboroda <smaiboroda@kse.org.ua>
2024-11-27 01:33:35 +0200

    додано функцію для читання програмою бази даних

--- main.py
+++ main.py
@@ -7,3 +7,28 @@ def parse_arguments():
     parser.add_argument('-total', type=int, help='Year to see country`s medals')
     parser.add_argument('-output', type=str, help='File to output results')
     return parser.parse_args()
+
+def read_data(file):
+    data = []
+    with open(file, 'r') as f:
+        table_headlines = f.readline()
+        for line in f:
+            row = line.strip().split('\t')
+            data.append({
+                'ID': row[0],
+                'Name': row[1],
+                'Sex': row[2],
+                'Age': row[3],
+                'Height': row[4],
+                'Weight': row[5],
+                'Team': row[6],
+                'NOC': row[7],
+                'Games': row[8],
+                'Year': row[9],
+                'Season': row[10],
+                'City': row[11],
+                'Sport': row[12],
+                'Event': row[13],
+                'Medal': row[14]
+            })
+    return data
